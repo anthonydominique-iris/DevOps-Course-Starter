cc085d607867ae37de44d38696d44e5bbadd480e
Anthony Dominique <anthony.dominique@iris.co.uk>
2021-11-30 19:21:26 +0000

    Only the index section done

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,11 +1,17 @@
 from flask import Flask
+from flask.templating import render_template
+from flask import session
+from todo_app.data.session_items import get_items
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()   
+    return render_template('index.html', items=items)
+    
+
+
